40ed645963262116d23241ebe09564cce58619f5
teconas <45254848+teconas@users.noreply.github.com>
2026-03-16 16:22:26 +0100

    Improve: mergec

--- commands/mergec.bat
+++ commands/mergec.bat
@@ -1,2 +1,15 @@
-:: Merge with forced commit
-git merge %* --no-ff
+:: Merge cleanly with forced commit
+@echo off
+SET SOURCE=%1
+SET EXTRA_ARGS=
+for /f "tokens=1,* delims= " %%a in ("%*") do set EXTRA_ARGS=%%b
+for /f %%i in ('git branch --show-current') do SET TARGET=%%i
+
+git merge-base --is-ancestor %TARGET% %SOURCE% >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo Error: Rebase %TARGET% into %SOURCE% first.
+	exit 1
+)
+
+@echo on
+git merge %SOURCE% --no-ff %EXTRA_ARGS%
